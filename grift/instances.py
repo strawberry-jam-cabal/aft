9525c9a547230709fd2fb2a6cbf417ce2fe49d1e
nbertagnolli <nmbertagnolli@gmail.com>
2018-12-19 17:30:52 -0700

    refactored instance generators into separate file
    Co-authored-by: Annie Cherkaev <annie.cherk@gmail.com>

---
+++ grift/instances.py
@@ -0,0 +1,56 @@
+"""
+This file holds instances of defined fuzz generators.  These methods are used
+to fuzz the python code.
+"""
+
+import numpy as np
+
+# todo iterative generation
+
+def get_ints():
+    # todo control how many you want to try
+    ints = [1, 0, -1]
+    ints += np.random.randint(-1000, 1000, 10).tolist()
+    return list(map(lambda x: ("int", x), ints))
+
+
+def get_floats():
+    floats = [-1.5, 0.0, 1.3]
+    floats += (np.random.rand(10) * 100).tolist()
+    return list(map(lambda x: ("float", x), floats))
+
+
+def get_bools():
+    bools = [True, False]
+    return list(map(lambda x: ("bool", x), bools))
+
+# todo, random strings & edge case strings (for type refinement)
+def get_strings():
+    strings = ["\"a\"", "\"\"",
+               "\"life is rolling on, and it's all very exciting\""]
+    return list(map(lambda x: ("string", x), strings))
+
+
+def make_identical_lists(x, typeAnnotation):
+    lists = [[x], [x] * 2, [x] * 5, [x] * 50, []]
+    fullType = "[" + typeAnnotation + "]"
+    return list(map(lambda x: (fullType, x), lists))
+
+
+def get_int_list():
+    return make_identical_lists(1, "int")
+
+
+def get_float_list():
+    return make_identical_lists(1.0, "float")
+
+
+def get_string_list():
+    return make_identical_lists("trust your silent captain", "string")
+
+
+# tuples, nested lists, mutiple arguments
+
+def get_instances():
+    return get_ints() + get_floats() + get_bools() \
+           + get_strings() + get_int_list() + get_float_list() + get_string_list()
